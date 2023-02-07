cdf11516beadc6a212dfee71420e6c8de616a1ed
Salamander01 <96356159+Salamander01@users.noreply.github.com>
2023-02-07 06:25:03 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,21 +8,24 @@ import static java.lang.Math.round;
 public class Main {
 
     final static boolean DEBUG = false;
-    final static float SPEED_MULTIPLIER = 0.4F;
+    final static float SPEED_MULTIPLIER = .25F;
     final static int BALL_RADIUS = 10;
 
     public static void main(String[] args) {
         MainAnimation animation = new MainAnimation();
         while (animation.getHeight() < 200 && animation.getWidth() < 200);
-//        int width = (int) round(animation.getWidth() / 40.0);
-//        int height = (int) round(animation.getHeight() / 40.0);
-//        animation.setSize(animation.getWidth() - (animation.getWidth() % 40), animation.getHeight() - (animation.getHeight() % 40));
-        int width = 24;
-        int height = 12;
+        animation.setSize(animation.getWidth() - (animation.getWidth() % 40), animation.getHeight() - (animation.getHeight() % 40));
+        int width = (int) round((animation.getWidth() - BALL_RADIUS) / 40.0);
+        int height = (int) round((animation.getHeight() - BALL_RADIUS) / 40.0);
+        if (DEBUG) {
+            System.out.println(width + " x " + height);
+            System.out.println(animation.getWidth() + " x " + animation.getHeight());
+            System.out.println(animation.getWidth() + " x " + animation.getHeight());
+        }
         for (int i = 1; i <= width; i++) {
             for (int j = 1; j <= height; j++) {
-                Color color = new Color(i * 10, 0, 255);
-                animation.addBall(BALL_RADIUS, 0, 0, (float) (i) * SPEED_MULTIPLIER,  (float) (j) * SPEED_MULTIPLIER, color);
+                Color color = new Color(i * (i / 255), 0, 255);
+                animation.addBall(BALL_RADIUS, BALL_RADIUS, BALL_RADIUS, (float) (i) * SPEED_MULTIPLIER,  (float) (j) * SPEED_MULTIPLIER, color);
             }
         }
         ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
